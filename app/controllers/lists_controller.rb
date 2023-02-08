0f56e71e6975b2685b2c10d0e36f947a69ce59d5
Lorenzo <lorenzo.bertolino@gmail.com>
2023-02-08 20:41:19 +0100

    until point 3

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,31 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+#1Q what are we doing exactly in the create? what are we rendering with the :new?
+#2Q what are we doing exactly in the list_params?
+end
